99d283bf2d795c6163a0ce22f16ce206488719b0
andreeazgarcea <andreeazgarcea31@gmail.com>
2021-10-30 10:10:55 +0300

    Initial commit

---
+++ curs/src/tema.java
@@ -0,0 +1,10 @@
+public class tema {
+
+
+    public static void Hello (){
+        System.out.println("salutare+Andreea");
+        System.out.println(temp.lenght()+"-Nr total de caractere din sir"+"\n Sirul este"+temp)"
+
+
+    }
+}
